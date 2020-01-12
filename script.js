e4ffa59d896a7e34561f9a7d805e6679e9a19ca1
AushaL <thetingler85@gmail.com>
2020-01-12 13:22:50 +0300

    Закончил прототип проекта

---
+++ script.js
@@ -0,0 +1,22 @@
+// Сохранение данных
+
+var todos = ['item 1', 'item 2', 'item 3'];
+
+// Вывод данных
+
+console.log(todos);
+
+// Добавление новых данных
+
+todos.push('item 4');
+console.log(todos);
+
+// Изменение данных 
+
+todos[0] = 'start';
+console.log(todos);
+
+// Удаление данных
+
+todos.splice(0, 1);
+console.log(todos);
