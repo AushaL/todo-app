fc2c339e98e2b2190737c8e426ba668dfd89354f
AushaL <thetingler85@gmail.com>
2020-01-13 15:58:46 +0300

    Добавил функцию для вывода данных в консоль

--- script.js
+++ script.js
@@ -4,19 +4,22 @@ var todos = ['item 1', 'item 2', 'item 3'];
 
 // Вывод данных
 
-console.log(todos);
+function displayTodos() {
+    console.log('My todos: ', todos);
+}
+
+displayTodos()
 
 // Добавление новых данных
 
 todos.push('item 4');
-console.log(todos);
 
 // Изменение данных 
 
 todos[0] = 'start';
-console.log(todos);
 
 // Удаление данных
 
 todos.splice(0, 1);
-console.log(todos);
+
+
