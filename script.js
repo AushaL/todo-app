aab5bae67e3770d10345c056d96c3fd6b8b50f29
AushaL <thetingler85@gmail.com>
2020-01-15 13:46:05 +0300

    Добавил функции удаления и добавления данных массива в объект

--- script.js
+++ script.js
@@ -6,29 +6,21 @@ var todoList = {
     addNewTodo: function(todo) {  // Добавление новых данных
         this.todos.push(todo);
         this.displayTodos();
+    },
+    changeTodo: function(position, newValue) { // Изменение данных 
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) { // Удаление данных
+        this.todos.splice(position, 1);
+        this.displayTodos();
     }
 };
 
 todoList.displayTodos();
 todoList.addNewTodo('item 4');
-
-// Изменение данных 
-
-function changeTodo(position, newValue) {
-    todos[position] = newValue;
-    displayTodos();
-}
-
-changeTodo(2, 'new todo');
-
-// Удаление данных
-
-function deleteTodo(position) {
-    todos.splice(position, 1);
-    displayTodos();
-}
-
-deleteTodo(1);
+todoList.changeTodo(2, 'new todo');
+todoList.deleteTodo(2);
 
 
 
