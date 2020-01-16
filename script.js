d322d933af29b10da42d824364f2f2330c73db6a
AushaL <thetingler85@gmail.com>
2020-01-16 15:32:43 +0300

    Добавил объекты в массив и метод для переключения булевых значений

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 var todoList = {
-    todos: ['item 1', 'item 2', 'item 3'], // Сохранение данных
+    todos: [], // Сохранение данных
     displayTodos: function() {
         console.log('My todos: ', this.todos); // Вывод данных
     },
-    addNewTodo: function(todo) {  // Добавление новых данных
-        this.todos.push(todo);
+    addNewTodo: function(todoText) {  // Добавление новых данных
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
         this.displayTodos();
     },
-    changeTodo: function(position, newValue) { // Изменение данных 
-        this.todos[position] = newValue;
+    changeTodo: function(position, todoText) { // Изменение данных 
+        this.todos[position].todoText = todoText;
         this.displayTodos();
     },
     deleteTodo: function(position) { // Удаление данных
         this.todos.splice(position, 1);
         this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
     }
 };
 
-todoList.displayTodos();
 todoList.addNewTodo('item 4');
-todoList.changeTodo(2, 'new todo');
-todoList.deleteTodo(2);
+todoList.changeTodo(0, 'new todo');
+// todoList.deleteTodo(2);
 
+todoList.toggleCompleted(0);
 
 
 
