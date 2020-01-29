b38fe2a56b814bd24a492a921a2989fa892b1cd9
AushaL <thetingler85@gmail.com>
2020-01-29 18:46:00 +0300

    Добавил вывод результатов действия пользователя на экран

--- script.js
+++ script.js
@@ -1,38 +1,20 @@
 var todoList = { // 0. Объект включает в себя весь функционал программы
     todos: [], // 1. Массив в который сохраненяются данные
-    displayTodos: function() { // 2. Метод выводящий данные из массива todo в консоль
-        if (this.todos.length === 0) {
-            console.log('Your todo list is empty!'); // Если массив пустой
-        } else {
-            console.log('My todos:'); 
-            for (var i = 0; i < this.todos.length; i++) { // Если массив не пустой
-                if (this.todos[i].completed === true) {
-                    console.log('(x)', this.todos[i].todoText); 
-                } else {
-                    console.log('( )', this.todos[i].todoText);
-                }
-            }
-        }
-    },
     addNewTodo: function(todoText) {  // 3. Добавление новых данных в массив
         this.todos.push({
             todoText: todoText,
             completed: false
         });
-        this.displayTodos();
     },
     changeTodo: function(position, todoText) { // 4. Изменение данных 
         this.todos[position].todoText = todoText;
-        this.displayTodos();
     },
     deleteTodo: function(position) { // 5. Удаление данных
         this.todos.splice(position, 1);
-        this.displayTodos();
     },
     toggleCompleted: function(position) { // 6. Меняем состояние "Выполнено" на противоположное 
         var todo = this.todos[position];
         todo.completed = !todo.completed;
-        this.displayTodos();
     },
     toggleAll: function() { // 7. Меняем свойство "Выполнено" всех объектов в массиве на противоположное
         var totalTodos = this.todos.length; 
@@ -51,19 +33,15 @@ var todoList = { // 0. Объект включает в себя весь фун
                 this.todos[i].completed = true;
             }
         }
-
-        this.displayTodos();
     }
 };
 
 var handlers = {
-    displayTodos: function() {
-        todoList.displayTodos();
-    },
     addTodo: function() {
         var addTodoTextInput = document.getElementById('addTodoTextInput');
         todoList.addNewTodo(addTodoTextInput.value);
         addTodoTextInput.value = "";
+        view.displayTodos();
     },
     changeTodo: function () {
         var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
@@ -71,73 +49,52 @@ var handlers = {
         todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
         changeTodoPositionInput.value = "";
         changeTodoTextInput.value = "";
+        view.displayTodos();
     },
     deleteTodo: function () {
         var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
         todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
         deleteTodoPositionInput.value = "";
+        view.displayTodos();
     },
     toggleCompleted: function () {
         var toggleCompletedPositionInput = document.getElementById("toggleCompletedPositionInput");
         todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
         toggleCompletedPositionInput.value = "";
+        view.displayTodos();
     },
     toggleAll: function() {
         todoList.toggleAll();
+        view.displayTodos();
     }
 };
 
-// facebook clone
+var view = {
+    displayTodos: function () {
+        var todosUl = document.querySelector('ul');
+        todosUl.innerHTML = '';
+        for (var i = 0; i < todoList.todos.length; i++) {
+            var todoLi = document.createElement('li');
+            var todo = todoList.todos[i];
+            var todoTextWithCompletion = '';
 
-// var dataBase = [
-//     {
-//         userName: "Tom",
-//         password: "666"
-//     },
-//     {
-//         userName: "Sally",
-//         password: "123"
-//     },
-//     {
-//         userName: "Ingrid",
-//         password: "555"
-//     }
-// ];
+            if (todo.completed === true) {
+                todoTextWithCompletion = '(x) ' + todo.todoText;
+            } else {
+                todoTextWithCompletion = '( ) ' + todo.todoText;
+            }
 
-// var newsFeed = [
-//     {
-//         userName: "Bob",
-//         timiLine: "So tired from all that learning"
-//     },
-//     {
-//         userName: "Sally",
-//         timiLine: "I hate my shitty life"
-//     },
-//     {
-//         userName: "Mike",
-//         timiLine: "Js is preety cool!"
-//     }
-// ];
+            todoLi.textContent = todoTextWithCompletion;
+            todosUl.appendChild(todoLi);
+        }
+    }
+};
 
-// function isUserValid(userName, password) {
-//     for (var i = 0; i < dataBase.length; i++) {
-//         if (dataBase[i].userName === userName &&
-//             dataBase[i].password === password) {
-//                 return true;
-//         }
-//     }
-//     return false;
-// }
+var a;
+console.log(a);
 
-// function signIn(userName, password) {
-//     if (isUserValid(userName, password)) {
-//         console.log(newsFeed);
-//     } else {
-//         alert("Sorry, wrong username or password");
-//     }
-// }
-
-// var userNamePrompt = prompt("What's your user name?");
-// var passwordPrompt = prompt("What's your password?");
- 
-// signIn(userNamePrompt, passwordPrompt);
+if (a === undefined) {
+    console.log('a is undefined!');
+} else {
+    console.log('a is defined!');
+}
